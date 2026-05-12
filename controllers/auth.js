9a1c0a6d8b37756ab4e1523405abbe671f917496
Aghisa Christyananda <aghisachristynanda434@gmail.com>
2026-05-12 10:19:48 +0700

    fix css and auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,3 @@
-// Controller: Auth
-// Dipakai di login.html dan register-rs.html
-
-// Toggle password visibility - password utama
 document.getElementById('togglePassword').addEventListener('click', function () {
     const input = document.getElementById('password');
     const isHidden = input.type === 'password';
